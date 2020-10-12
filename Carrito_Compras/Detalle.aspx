5347d465eb52cd894f28e6ad45efa7040570944b
Cristian Paolini <cristianpaolini3@gmail.com>
2020-10-12 18:53:56 -0300

    Agregué Detalle.aspx y Detalle.aspx.cs junto al código correspondiente. Queda chequear por qué da null reference en articuloDetalle..

---
+++ Carrito_Compras/Detalle.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="Detalle" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Detalle.aspx.cs" Inherits="Carrito_Compras.Detalle" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="jumbotron">
+        <img src ="<% = articuloDetalle.ImagenUrl %>" class="card-img-top" alt="Imagen no disponible"; />
+        <h1 class="display-4"><% = articuloDetalle.Nombre %></h1>
+        <p class="lead"><% = articuloDetalle.Precio %></p>
+        <hr class="my-4">
+        <p><% = articuloDetalle.Descripcion %></p>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ Carrito_Compras/Detalle.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
+
+namespace Carrito_Compras
+{
+    public partial class Detalle : System.Web.UI.Page
+    {
+        public Articulo articuloDetalle { get; set; }
+
+        public List<Articulo> listaArticulosCarrito { get; set; }
+        Dictionary<int, int> listaCarrito = new Dictionary<int, int>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            List<Articulo> listaAux;
+            try
+            {
+                listaAux = negocio.listar();
+                int idAux = Convert.ToInt32(Request.QueryString["idArticulo"]);
+                articuloDetalle = listaAux.Find(x => x.Id == idAux);
+
+                if (listaCarrito == null)
+                {
+                    listaCarrito = new Dictionary<int, int>();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Session.Add("Session_id_" + Session.SessionID + "_error", ex.Message);
+                Response.Redirect("Error.aspx");
+            }
+        }
+    }
+}
