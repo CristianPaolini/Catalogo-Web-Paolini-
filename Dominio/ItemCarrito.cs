5853829e524e37cf8f9114ebda0857fa03f1ccfd
Cristian Paolini <cristianpaolini3@gmail.com>
2020-10-15 00:46:35 -0300

    Añadí las clases referidas al manejo de items en el carrito y artículos seleccionados

---
+++ Dominio/ItemCarrito.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class ItemCarrito
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string ImagenUrl { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public int Cantidad { get; set; }
+        public SqlMoney PrecioUnitario { get; set; }
+    }
+}
